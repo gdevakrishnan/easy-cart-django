b7e871066903d2db8fef56599154e6f2f090f9b7
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-02-20 21:20:11 +0530

    App created and connected to the project

---
+++ easy_cart_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EasyCartAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'easy_cart_app'
--- easy_cart_project/settings.py
+++ easy_cart_project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'easy_cart_app' # app connected to the installed apps
 ]
 
 MIDDLEWARE = [
