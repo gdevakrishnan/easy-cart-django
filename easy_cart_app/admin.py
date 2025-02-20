9dbfb5e696e287b85c097a1279267bf012027579
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-02-20 23:03:16 +0530

    Update the category list titles of admin

--- easy_cart_app/admin.py
+++ easy_cart_app/admin.py
@@ -3,6 +3,10 @@ from .models import Category
 from .models import Product
 # from .models import *
 
+# to alter the lists in the admin page
+class CategoryAdmin (admin.ModelAdmin):
+    list_display = ('name', 'description')
+
 # Register your models here.
-admin.site.register(Category)
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Product)
