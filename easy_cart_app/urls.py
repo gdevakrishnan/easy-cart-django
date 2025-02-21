2ac026e43cd5d6d13e6351c6e6945e04301da350
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-02-21 12:57:04 +0530

    View products from category wise

--- easy_cart_app/urls.py
+++ easy_cart_app/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('register', views.register, name='register'),
     path('collections', views.collections, name='collections'),
+    path('collections/<str:name>', views.collections_view, name='collections'),
     path('features', views.features, name='features')
 ]
--- easy_cart_app/views.py
+++ easy_cart_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
 
 def home (request):
@@ -10,6 +11,14 @@ def register (request):
 def collections (request):
     category = Category.objects.filter(status = 0)
     return render(request, 'shop/collections.html', {"category": category})
+
+def collections_view (request, name):
+    if (Category.objects.filter(name = name, status = 0)):
+        products = Product.objects.filter(category__name = name)
+        return render(request, 'shop/products/index.html', { "products": products, "category": name })
+    else:
+        messages.warning(request, "No such category found")
+        return redirect('collections')
 
 def features (request):
     return render(request, 'shop/features.html')
