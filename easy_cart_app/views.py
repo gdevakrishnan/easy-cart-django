e5437b2f36ea6aa3a58ec5d7796a7ef0e3f53c57
gdevakrishnan <gdevakrishnan700@gmail.com>
2025-02-21 10:33:43 +0530

    Add the media urls to the main urls in project to load media files

--- easy_cart_app/views.py
+++ easy_cart_app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
+from .models import *
 
 def home (request):
     return render(request, 'shop/index.html')
 
 def register (request):
     return render(request, 'shop/register.html')
+
+def collections (request):
+    category = Category.objects.filter(status = 0)
+    return render(request, 'shop/collections.html', {"category": category})
 
 def features (request):
     return render(request, 'shop/features.html')
